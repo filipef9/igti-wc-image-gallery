cd30647156bfe7863e871be483a4ca2e773dc1a0
filipef9 <filipef9@hotmail.com>
2021-09-20 11:02:48 +0000

    Refactoring e correção de problemas.

--- js/img-gallery.js
+++ js/img-gallery.js
@@ -38,7 +38,17 @@ class ImgGalleryElement extends HTMLElement {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.append(tpl.content.cloneNode(true));
 
-        this.selectedIdx = 0;
+        let selectedIdx = 0;
+        Object.defineProperty(this, 'selectedIdx', {
+            get() {
+                return selectedIdx;
+            },
+            set(value) {
+                const imgs = imgsEl.assignedNodes().filter(isImg);
+                selectedIdx = fixIndex(value, imgs.length);
+                updateUi();
+            }
+        })
 
         const imgsEl = this.shadowRoot.getElementById('imgs');
         const currentEl = this.shadowRoot.getElementById('current');
@@ -46,30 +56,49 @@ class ImgGalleryElement extends HTMLElement {
         const nextEl = this.shadowRoot.getElementById('next');
 
         const updateUi = () => {
-            const imgs = imgsEl.assignedNodes().filter(e => e.nodeName === 'IMG');
-            currentEl.textContent = `Foto ${this.selectedIdx + 1} de ${imgs.length}`;
+            const imgs = imgsEl.assignedNodes().filter(isImg);
+            currentEl.textContent = `Foto ${selectedIdx + 1} de ${imgs.length}`;
 
             for (let i = 0; i < imgs.length; i++) {
                 const img = imgs[i];
-                img.style.display = this.selectedIdx === i ? 'initial' : 'none';
+                img.style.display = selectedIdx === i ? 'initial' : 'none';
             }
 
-            prevEl.disabled = this.selectedIdx === 0;
-            nextEl.disabled = this.selectedIdx === (imgs.length - 1);
+            prevEl.disabled = selectedIdx === 0;
+            nextEl.disabled = selectedIdx === (imgs.length - 1);
         }
 
         prevEl.addEventListener('click', () => {
             this.selectedIdx--;
-            updateUi();
         });
 
         nextEl.addEventListener('click', () => {
             this.selectedIdx++;
-            updateUi();
+        });
+
+        imgsEl.addEventListener('slotChange', () => {
+            this.selectedIdx = this.selectedIdx;
+            // const imgs = imgsEl.assignedNodes().filter(isImg);
+            // selectedIdx = fixIndex(selectedIdx, imgs.length);
+            // updateUi();
         });
 
         updateUi();
     }
+}
+
+const fixIndex = (index, length) => {
+    if (index >= length) {
+        return length - 1;
+    } else if (index < 0) {
+        return 0;
+    } else {
+        return index;
+    }
+};
+
+const isImg = (e) => {
+    return e.nodeName === 'IMG';
 }
 
 customElements.define('img-gallery', ImgGalleryElement);
